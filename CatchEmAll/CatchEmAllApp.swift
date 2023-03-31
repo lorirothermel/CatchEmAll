d9407cb508f509f2889ac69758dfddfac3d8a1b3
LBRothermel <128320877+LBRothermel@users.noreply.github.com>
2023-03-31 00:47:34 -0400

    Initial Commit

---
+++ CatchEmAll/CatchEmAllApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatchEmAllApp.swift
+//  CatchEmAll
+//
+//  Created by Lori Rothermel on 3/31/23.
+//
+
+import SwiftUI
+
+@main
+struct CatchEmAllApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
